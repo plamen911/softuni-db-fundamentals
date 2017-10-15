c9ea10cc020f6dfb046d2b891a2b8efebf3b8182
apple <1>
2017-10-15 21:58:40 +0300

    17. Create View Employees with Job Titles ready

---
+++ DatabasesBasics/_03BasicCrud/_17CreateViewEmployeesWithJobTitles.sql
@@ -0,0 +1,5 @@
+CREATE VIEW v_employees_job_titles AS
+  SELECT
+    CONCAT(first_name, ' ', IF(middle_name IS NULL, '', middle_name), ' ', last_name) AS full_name,
+    job_title
+  FROM employees;
