04f9d43c86c55095b74d87f52f70a4de4c039aec
apple <1>
2017-10-16 18:57:43 +0300

    02. Employees with Salary Above Number ready

---
+++ DatabasesBasics/_08FunctionsTriggersAndTransactions/_02EmployeesWithSalaryAboveNumber.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE usp_get_employees_salary_above(salary_above DOUBLE)
+  BEGIN
+    SELECT
+      first_name,
+      last_name
+    FROM employees
+    WHERE salary >= salary_above
+    ORDER BY first_name, last_name, employee_id;
+  END
