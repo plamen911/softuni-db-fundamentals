64a652b6ea247a13127c9473c2fa95dad2c057b3
apple <1>
2017-10-16 22:33:15 +0300

    Amend last

--- DatabasesBasics/_08FunctionsTriggersAndTransactions/_10PeopleWithBalanceHigherThan.sql
+++ DatabasesBasics/_08FunctionsTriggersAndTransactions/_10PeopleWithBalanceHigherThan.sql
@@ -6,7 +6,7 @@ CREATE PROCEDURE usp_get_holders_with_balance_higher_than(amount DECIMAL(19, 4))
     FROM accounts AS a
       INNER JOIN account_holders AS ah
         ON a.account_holder_id = ah.id
-    GROUP BY first_name, last_name
+    GROUP BY ah.first_name, ah.last_name
     HAVING SUM(a.balance) > amount
     ORDER BY a.id;
   END
