50e7cd8be7d2dfa25b9ebf796bb84fa5ad17ee9f
apple <1>
2017-10-16 22:32:29 +0300

    10. People with Balance Higher Than ready

---
+++ DatabasesBasics/_08FunctionsTriggersAndTransactions/_10PeopleWithBalanceHigherThan.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE usp_get_holders_with_balance_higher_than(amount DECIMAL(19, 4))
+  BEGIN
+    SELECT
+      ah.first_name,
+      ah.last_name
+    FROM accounts AS a
+      INNER JOIN account_holders AS ah
+        ON a.account_holder_id = ah.id
+    GROUP BY first_name, last_name
+    HAVING SUM(a.balance) > amount
+    ORDER BY a.id;
+  END
