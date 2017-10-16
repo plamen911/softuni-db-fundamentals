df41721c9d27fc8ad5fe58c9d38f0304ff6213fe
apple <1>
2017-10-16 22:03:28 +0300

    07. Define Function ready

--- DatabasesBasics/_08FunctionsTriggersAndTransactions/_07DefineFunction.sql
+++ DatabasesBasics/_08FunctionsTriggersAndTransactions/_07DefineFunction.sql
@@ -1,7 +1,18 @@
 CREATE FUNCTION ufn_is_word_comprised(set_of_letters VARCHAR(50), word VARCHAR(50))
-  RETURNS TINYINT(1)
+  RETURNS TINYINT
   BEGIN
-    DECLARE result TINYINT(1);
-    SET result = (SELECT word REGEXP CONCAT('^[', set_of_letters, ']+$'));
-    RETURN result;
+    DECLARE counter INT;
+    DECLARE ch VARCHAR(255);
+    SET counter = 1;
+    WHILE counter < CHAR_LENGTH(word)
+    DO
+      SET ch = SUBSTRING(word, counter, 1);
+      IF (LOCATE(ch, set_of_letters, 1) = 0)
+      THEN
+        RETURN 0;
+      END IF;
+
+      SET counter = counter + 1;
+    END WHILE;
+    RETURN 1;
   END
